825dd1a9e42f47e1cfc125ed936c03da2fc28157
ilorez <zobirnajdaoui@gmail.com>
2024-12-03 10:35:42 +0100

    should return (nil) if ptr was null

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:35:18 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/12/02 15:38:17 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/12/03 09:36:05 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_print_ptr(unsigned long ptr)
 {
 	int	out_len;
 
+	if ((void *)ptr == NULL)
+		return (write(1, "(nil)", 5));
 	write(1, "0x", 2);
 	out_len = 2;
 	out_len += ft_print_hexa(ptr, "0123456789abcdef");
