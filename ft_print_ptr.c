120ee8e69c763a6e11c1a3f21eb91e6adfd1656a
ilorez <zobirnajdaoui@gmail.com>
2024-12-03 13:15:48 +0100

    woow forgot it

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,7 +6,7 @@
 /*   By: znajdaou <znajdaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 15:35:18 by znajdaou          #+#    #+#             */
-/*   Updated: 2024/12/03 13:02:41 by znajdaou         ###   ########.fr       */
+/*   Updated: 2024/12/03 13:15:32 by znajdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 
 int	ft_print_ptr(unsigned long ptr)
 {
-	int	out_len;
-
 	if ((void *)ptr == NULL)
 		return (write(1, "(nil)", 5));
 	return (write(1, "0x", 2) + ft_print_hexa(ptr, "0123456789abcdef"));
